cff1cce906f69c0cb9138f854230621f468b4d1a
KarimTamani <tamanikarim50@gmail.com>
2025-08-16 11:27:02 +0100

    Add NavBar

--- src/pages/dashboard/dashboard-page.tsx
+++ src/pages/dashboard/dashboard-page.tsx
@@ -1,6 +1,7 @@
 
-import Sidebar from "@/components/sidebar/sidebar"; 
 import Navbar from "@/components/navbar/navbar";
+import Sidebar from "@/components/sidebar/sidebar"; 
+ 
 import { Outlet } from "react-router-dom";
 
 
